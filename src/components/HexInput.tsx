2742e32d7a47d741b759ecd6044ec5f995068a4d
Patrick Lake <patricklake@PatrickLakesMac.attlocal.net>
2024-05-13 17:38:37 -0400

    Remove ts errors

--- src/components/HexInput.tsx
+++ src/components/HexInput.tsx
@@ -1,38 +1,38 @@
-import React, { useState, useEffect } from "react"
-import { convertHslToHex } from "../helpers/utils"
+// import React, { useState, useEffect } from "react"
+// import { convertHslToHex } from "../helpers/utils"
 
-/**
- * A component that renders an input box for hex color values.
- * It automatically triggers the `onSubmit` function when a valid hex color is entered.
- */
-const HexInput = ({
-  onSubmit,
-  hsla,
-}: {
-  onSubmit: (hex: string) => void
-  hsla: string
-}) => {
-  const [value, setValue] = useState(hsla)
+// /**
+//  * A component that renders an input box for hex color values.
+//  * It automatically triggers the `onSubmit` function when a valid hex color is entered.
+//  */
+// const HexInput = ({
+//   onSubmit,
+//   hsla,
+// }: {
+//   onSubmit: (hex: string) => void
+//   hsla: string
+// }) => {
+//   const [value, setValue] = useState(hsla)
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value)
-  }
+//   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     setValue(event.target.value)
+//   }
 
-  useEffect(() => {
-    if (/^#([0-9A-F]{3}){1,2}$/i.test(value)) {
-      onSubmit(value)
-    }
-  }, [value])
+//   useEffect(() => {
+//     if (/^#([0-9A-F]{3}){1,2}$/i.test(value)) {
+//       onSubmit(value)
+//     }
+//   }, [value])
 
-  return (
-    <input
-      className="input"
-      type="text"
-      value={hsla}
-      //   onChange={handleChange}
-      placeholder="Enter a hex color..."
-    />
-  )
-}
+//   return (
+//     <input
+//       className="input"
+//       type="text"
+//       value={hsla}
+//       //   onChange={handleChange}
+//       placeholder="Enter a hex color..."
+//     />
+//   )
+// }
 
-export default HexInput
+// export default HexInput
